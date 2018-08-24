0c2ec470a1c07012a45816e29ef07d22b5a81519
Jason Tsang <jason@Jasons-MacBook-Pro.local>
2018-08-23 23:48:17 -0700

    Clean-up

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-$(function() {
-  var images = ["jasontsang1.png", "jasontsang2.png", "jasontsang3.png", "jasontsang4.png", "jasontsang5.png", "jasontsang6.png"];
-  $("#banner").css({'content': 'url(/images/banner/' + images[Math.floor(Math.random() * images.length)] + ')'});
-    
+$(function() {  
   var mail = ["tsang", "jsn", 64, ".com", "mail", "g"];
   document.getElementById("eaddress").textContent = mail[1] + mail[0] + String.fromCharCode(mail[2]) + mail[5] + mail[4] + mail[3];
 });
