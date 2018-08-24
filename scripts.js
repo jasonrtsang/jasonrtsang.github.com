45f835f340610266a18d935af0d625884ff814e0
Jason Tsang <jasonrtsang@gmail.com>
2018-08-23 23:28:55 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(function() {
   var images = ["jasontsang1.png", "jasontsang2.png", "jasontsang3.png", "jasontsang4.png", "jasontsang5.png", "jasontsang6.png"];
   $("#banner").css({'content': 'url(/images/banner/' + images[Math.floor(Math.random() * images.length)] + ')'});
     
-  var mail = ["son", "ja", 64, "ang.com", "onrts", "jas"];
+  var mail = ["tsang", "jsn", 64, ".com", "mail", "g"];
   document.getElementById("eaddress").textContent = mail[1] + mail[0] + String.fromCharCode(mail[2]) + mail[5] + mail[4] + mail[3];
 });
 
